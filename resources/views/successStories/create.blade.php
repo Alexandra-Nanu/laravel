ef0933cc422be4190f0d0cea22c5ae4a76064ffa
alexa <alexa@ALEXANDRA_NANU>
2024-12-15 10:06:22 +0200

    blade-uri

---
+++ resources/views/successStories/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+@section('content')
+
+
+<form action="{{ route('successStories.store') }}" method="POST">
+    @csrf
+    <label for="title">Title:</label>
+    <input type="text" name="title" required><br>
+    <label for="story">Story:</label>
+    <textarea name="story" required> </textarea> <br>
+    <label for="member_id">Member Name:</label>
+    <input type="text" name="member_id" required><br>
+    <br>
+    <button type="submit">Add Story</button>
+</form>
+
+
+@endsection
